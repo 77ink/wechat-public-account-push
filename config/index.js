2331cb524576089942dfbd796c8d2a1e42c39da5
77ink <114296524+77ink@users.noreply.github.com>
2022-09-24 22:11:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '17c1dd2c41801633e1818434beead9b4',
+  APP_SECRET: '7b190b25b90aa18c35556523d8ad0aff',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
